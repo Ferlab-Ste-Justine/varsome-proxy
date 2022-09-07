3035e51ca153da9bfa1f1f66c79c122c6c397cee
Matt Syms <matthieu.symoens@gmail.com>
2022-09-07 08:35:33 -0400

    feat: CLIN-1328 added iso month and day utils

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,5 @@ import parseDuration from 'parse-duration';
 export const formatBytes = (n: number) => bytes(n).toLowerCase();
 export const parseBytes = (s: string) => bytes(s);
 export const parseMillis = (s: string) => parseDuration(s);
+export const isoDay = () => new Date().toISOString().substring(0, 10);
+export const isoMonth = () => new Date().toISOString().substring(0, 7);
